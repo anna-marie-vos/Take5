e1b3489170bb6f97aa9cf316e746db3cfbc65fec
anna-marie-vos <amvos@outlook.com>
2016-12-14 12:01:18 +1300

    initial setup working, handlebars works

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ app.set('view engine', 'handlebars') //causes the render function to work
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+// stuff above are all setup type things
+
+app.get('/',function(req,res){
+  res.redirect('/index')
+})
+
+app.get('/index', function(req,res){
+  res.render('index')
+})
+
 
 
 module.exports = app
