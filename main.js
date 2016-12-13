908aba45db687d04bf00df72aa54af01379ecc7f
anna-marie-vos <amvos@outlook.com>
2016-12-14 11:33:42 +1300

    server setup and main.js code added

---
+++ main.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var expresshbs = require('express-handlebars')
+var path = require('path')
+var bodyParser = require('body-parser')
+var fs = require('fs')
+var app = express()
+
+// view engine setup
+app.engine('handlebars', expresshbs({defaultLayout: 'main'})) // makes the main page html file work.
+app.set('view engine', 'handlebars') //causes the render function to work
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
