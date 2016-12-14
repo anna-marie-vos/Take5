f48967cbfc8d83d061d08f7ff4c19fe5a3918cd3
anna-marie-vos <amvos@outlook.com>
2016-12-14 15:10:43 +1300

    database named hazardLog added with empty columns

--- migrations/20161214124309_hazardLog.js
+++ migrations/20161214124309_hazardLog.js
@@ -1,8 +1,21 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('hazardLog', function (table) {
+    table.increments();
+    table.string('project_site');
+    table.string('user');
+    table.string('hazard');
+    table.string('photos');
+    table.string('add_notice');
+    table.string('mitigation');
+    table.string('owner');
+    table.string('liklihood');
+    table.string('consequence');
+    table.boolean('eliminated');
+    table.timestamps();
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('hazardLog');
 };
