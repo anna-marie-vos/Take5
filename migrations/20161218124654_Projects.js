20eb70028102ddd24cb59c09e155dcf043154d9e
anna-marie-vos <amvos@outlook.com>
2016-12-20 09:56:08 +1300

    projects table project_site changed to project_name

--- migrations/20161218124654_Projects.js
+++ migrations/20161218124654_Projects.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('projects', function (table) {
     table.increments('project_id');
-    table.string('project_site');
+    table.string('project_name');
     table.string('location');
     table.string('SWMS');
     table.string('important_Notices');
--- seeds/makeProjectsList.js
+++ seeds/makeProjectsList.js
@@ -6,19 +6,19 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('projects').insert({project_id: 1,
-              project_site: 'Aurecon Offices',
+              project_name: 'Aurecon Offices',
               location:'Level 8, 1 Willis street,',
               SWMS:'Not available at present',
               important_Notices:''
             }),
         knex('projects').insert({project_id: 2,
-              project_site: 'Chorus exchange',
+              project_name: 'Chorus exchange',
               location:'wellington, 3 thorndon street',
               SWMS:'available, link required',
               important_Notices:'Only wooden ladders allowed on site'
             }),
         knex('projects').insert({project_id: 3,
-              project_site: 'GCNZ',
+              project_name: 'GCNZ',
               location:'Christchurch central',
               SWMS:'Available',
               important_Notices:'no personnel allowed on level 2 & 3'
